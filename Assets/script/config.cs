041b6d6415dd35e025cad33ec3dd03c9fea1db24
RAJESH PUJARA <13rp42@queensu.ca>
2020-02-29 01:01:29 -0500

    externalized some configs

---
+++ Assets/script/config.cs
@@ -0,0 +1,18 @@
+
+using System;
+namespace plats.Assets.script
+{
+    public class configVals{
+
+        public float jumpVelocity;
+        public float fallMultiplier;
+        public float lowJumpMultiplier;
+        public configVals(){
+            jumpVelocity = 5.0f;
+            fallMultiplier = 2.5f;
+            lowJumpMultiplier = 2.0f;
+        }
+
+    }
+
+}
--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using plats.Assets.script;
 
 public class playerMovement : MonoBehaviour {
 
 	Rigidbody2D rb2d;
+
+	//object which contains all externalized configs
+	configVals configInterface = new configVals();
 	//layer for all ground objects for jumping
 	public LayerMask groundLayer;
 	//player's box collider
@@ -12,34 +16,32 @@ public class playerMovement : MonoBehaviour {
 	//bool for checking if the player inputed jump on a given frame
 	bool jump;
 	//float for storing horizontal axis value
-	float m;
+	float xAxisVal;
 	//how fast the player will jump
-	public float jumpVelocity = 10;
+	public float jumpVelocity;
 	//how much faster the player will fall relative to rising
-	public float fallMultiplier = 2.5f;
+	public float fallMultiplier;
 	//how much shorter the short jump will be than the tall jump
-	public float lowJumpMultiplier = 2f;
+	public float lowJumpMultiplier;
 	//reference for storing the sprite of the player
 	SpriteRenderer playerSprite;
 	//the value for storing the oyxgen level, default set to full or max
 	public float globalBrightness = 1.0f;
 	//the value that the player's velocity will be assignned to when moving on the ground
-	Vector2 movec = new Vector2(5f, 0f);
+	Vector2 playerMoveSpeedVector = new Vector2(5f, 0f);
 	//the factor by which the player's mobility (amount that they can influence their speed) will be reduced in the air
-	public float airmovemult = 15.0f;
-	//check to see if the player is stuck to a wall
-	bool wallstuck = false;
+	public float airMoveMult = 15.0f;
 	//the sprite for the background
 	public SpriteRenderer bckg;
 
-	//check to see if the fishinghook is currently flying
-	bool isFish = false;
+	//check to see if the grapple is currently flying
+	bool isGrappling = false;
 	//empty list for counting the number of an object
-	GameObject[] gos;
+	GameObject[] gameObjects;
 	//object to use as hook
-	public GameObject Hook;
+	public GameObject grappleObj;
 	//bool to see whether or not to fire hook
-	bool isFiring = false;
+	bool fireGrapple = false;
 	//which direction the player is facing
 	public float facing;
 	//referencing animator
@@ -52,43 +54,37 @@ public class playerMovement : MonoBehaviour {
 		//fetch player sprite from unity object
 		playerSprite = GetComponent<SpriteRenderer>();
 		playerCollider = GetComponent<BoxCollider2D>();
-
+		jumpVelocity = configInterface.jumpVelocity;
+		fallMultiplier = configInterface.fallMultiplier;
+		lowJumpMultiplier = configInterface.lowJumpMultiplier;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-
-
-
-
 		//check for left and right movement
-		m = Input.GetAxisRaw("Horizontal");
-		anima.SetFloat("Blend", m);
+		xAxisVal = Input.GetAxisRaw("Horizontal");
+		anima.SetFloat("Blend", xAxisVal);
 
-		if (m != 0)
-			facing = m;
+		if (xAxisVal != 0)
+			facing = xAxisVal;
 
 
 		//check for grounded and if the jump button is being pressed
-		if (IsGrounded() && Input.GetButtonDown("Jump") && !isFish){
+		if (IsGrounded() && Input.GetButtonDown("Jump") && !isGrappling){
 		jump = true;
 		}
-
-
-				//if shooting check to see when you can give player control back
-		if(isFish){
-			gos = GameObject.FindGameObjectsWithTag("grapple");
-			if (gos.Length == 0)
-				isFish = false;
+		//if shooting check to see when you can give player control back
+		if(isGrappling){
+			gameObjects = GameObject.FindGameObjectsWithTag("grapple");
+			if (gameObjects.Length == 0)
+				isGrappling = false;
 		}
 
 		//check for fire
-		if(Input.GetButtonDown("Fire") && !isFish){
+		if(Input.GetButtonDown("Fire") && !isGrappling){
 
-			isFish = true;
-			isFiring = true;
+			isGrappling = true;
+			fireGrapple = true;
 
 		}
 
@@ -124,18 +120,18 @@ public class playerMovement : MonoBehaviour {
 		
 		//move left and right on the ground
 		if(IsGrounded()){
-			rb2d.velocity = movec * m;
+			rb2d.velocity = playerMoveSpeedVector * xAxisVal;
 			anima.SetFloat("Grounded", 1f);
 		}
 		//aerial movement
 		else{
 			anima.SetFloat("Grounded", 0f);
-			if(m > 0)
-				if(rb2d.velocity.x <= 0 || rb2d.velocity.x < movec.x)
-					rb2d.velocity += (movec*m)/airmovemult;
-			if(m < 0)
-				if (rb2d.velocity.x >= 0 || rb2d.velocity.x > -movec.x)
-					rb2d.velocity += (movec*m)/airmovemult;
+			if(xAxisVal > 0)
+				if(rb2d.velocity.x <= 0 || rb2d.velocity.x < playerMoveSpeedVector.x)
+					rb2d.velocity += (playerMoveSpeedVector*xAxisVal)/airMoveMult;
+			if(xAxisVal < 0)
+				if (rb2d.velocity.x >= 0 || rb2d.velocity.x > - playerMoveSpeedVector.x)
+					rb2d.velocity += (playerMoveSpeedVector*xAxisVal)/airMoveMult;
 		}
 		//preform jump
 		if (jump)
@@ -148,12 +144,12 @@ public class playerMovement : MonoBehaviour {
 		else if (rb2d.velocity.y > 0 && !Input.GetButton("Jump"))
 			rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 
-		if(isFiring){
-			GameObject newhook = Object.Instantiate(Hook, transform.position, Quaternion.identity);
+		if(fireGrapple){
+			GameObject newhook = Object.Instantiate(grappleObj, transform.position, Quaternion.identity);
 			Vector2 direction = new Vector2(10*facing,0);
 			direction.Normalize();
 			newhook.GetComponent<Rigidbody2D>().velocity = direction*20;
-			isFiring = false;
+			fireGrapple = false;
 		}
 	    jump = false;
 
